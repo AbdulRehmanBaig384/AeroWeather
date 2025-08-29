26e0ac024de00a18841fb2eac3b799bd39159b71
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-08-29 14:18:32 +0500

    Update api.js

--- AeroWeather/assest/js/api.js
+++ AeroWeather/assest/js/api.js
@@ -1,13 +1,11 @@
 'use strict';
 
-const apiKey= "511c0d53e786d6e701870951d85c605d";
-
+const apiKey= "YOUR API KEY";
 export const fetchData = (URL,callback)=>{
     fetch(`${URL}&appid=${apiKey}`)
     .then(res=>res.json())
     .then(data=>callback(data))
 }
-
 export const url ={
     currentWeather(lat,lon){
         return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
@@ -21,9 +19,7 @@ export const url ={
     reverseGeo(lat,lon){
         return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
-    /**
-     * @param {string} query search query e.g. :"london" , "New Yourk"  
-     */
+  
     geo(query){
         return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
